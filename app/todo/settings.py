5bbd560958972f2ac6a792745aa7e6265b9eeb03
Kyriakos <kiriakos.sdr@gmail.com>
2023-07-08 21:12:15 +0300

    Set up for deployment

--- app/todo/settings.py
+++ app/todo/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = "django-insecure-p-wwrcp1n8992l)zbp4p%(ym8+ggyt1n^vf_cjl_@-h2z0f^#j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['']
 
 # Application definition
 
@@ -64,14 +64,13 @@ WSGI_APPLICATION = "todo.wsgi.app"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
-DATABASES ={}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
