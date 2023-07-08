92da5542a922a616994a5a70ae8258d1bd3dc720
Kyriakos <kiriakos.sdr@gmail.com>
2023-07-08 20:39:39 +0300

    Finish basic operations

--- app/todo/settings.py
+++ app/todo/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -12,7 +13,7 @@ SECRET_KEY = "django-insecure-p-wwrcp1n8992l)zbp4p%(ym8+ggyt1n^vf_cjl_@-h2z0f^#j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -110,6 +111,8 @@ STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATIC_ROOT =os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
